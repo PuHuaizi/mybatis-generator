88cb68078e8b1bd2442438efd26d6cad624823c8
PuHuaizi <654195797@qq.com>
2019-04-22 16:30:45 +0800

    仓库初始化提交

---
+++ src/main/java/com/huatek/HelloSpringController.java
@@ -0,0 +1,28 @@
+package com.huatek;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author puxiongxiong
+ * @date 2019年4月19日
+ */
+@Controller
+public class HelloSpringController {
+	String message = "Welcome to Spring MVC!";
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "Spring") String name) {
+
+//		指定视图
+		ModelAndView mv = new ModelAndView("hellospring");
+//		向视图中添加所要的展示或使用的内容，将在页面中使用
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
---
+++ src/main/java/com/huatek/model/User.java
@@ -0,0 +1,93 @@
+package com.huatek.model;
+
+public class User {
+    private Integer id;
+
+    private String createTime;
+
+    private String lastLoginName;
+
+    private String loginName;
+
+    private String password;
+
+    private String realName;
+
+    private String role;
+
+    private Integer age;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime == null ? null : createTime.trim();
+    }
+
+    public String getLastLoginName() {
+        return lastLoginName;
+    }
+
+    public void setLastLoginName(String lastLoginName) {
+        this.lastLoginName = lastLoginName == null ? null : lastLoginName.trim();
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName == null ? null : loginName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName == null ? null : realName.trim();
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role == null ? null : role.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+}
